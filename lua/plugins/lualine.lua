096f5fab38ddb7ffb6f05da9d2301a90e2459519
leslie <noah.gr.addino@gmail.com>
2024-07-03 15:56:26 +0200

    up

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,76 @@
+return
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = function ()
+			require('lualine').setup {
+				options = {
+					icons_enabled = true,
+					theme = 'auto',
+					component_separators = { left = '', right = ''},
+					section_separators = { left = '', right = ''},
+					disabled_filetypes = {
+						statusline = {},
+						winbar = {},
+					},
+					ignore_focus = {},
+					always_divide_middle = true,
+					globalstatus = false,
+					refresh = {
+						statusline = 1000,
+						tabline = 1000,
+						winbar = 1000,
+					}
+				},
+				sections = {
+					lualine_a = {
+						{
+							'mode',
+							icons_enabled = true, -- Enables the display of icons alongside the component.
+							icon = nil,
+							separator = nil,      -- Determines what separator to use for the component.
+							cond = nil,           -- Condition function, the component is loaded when the function returns `true`.
+							draw_empty = false,   -- Whether to draw component even if it's empty.
+							color = nil, -- The default is your theme's color for that section and mode.
+							type = nil,
+							padding = 1, -- Adds padding to the left and right of components.
+							fmt = nil,   -- Format function, formats the component's output.
+							on_click = nil, -- takes a function that is called when component is clicked with mouse.
+						}
+					},
+					lualine_b = {'branch', 'diff', 'diagnostics'},
+					lualine_c = {
+						{
+							'filename',
+							file_status = true,      -- Displays file status (readonly status, modified status)
+							newfile_status = false,  -- Display new file status (new file means no write after created)
+							path = 3,                -- 0: Just the filename
+							shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
+							symbols = {
+								modified = '[+]',     -- Text to show when the file is modified.
+								readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+								unnamed = '[No Name]', -- Text to show for unnamed buffers.
+								newfile = '[New]',     -- Text to show for newly created file before first write
+							}
+						}
+					},
+					lualine_x = {'encoding', 'fileformat', 'filetype'},
+					lualine_y = {'progress'},
+					lualine_z = {'location'}
+				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = {'filename'},
+					lualine_x = {'location'},
+					lualine_y = {},
+					lualine_z = {}
+				},
+				tabline = {},
+				winbar = {},
+				inactive_winbar = {},
+				extensions = {}
+			}
+		end
+}
+
