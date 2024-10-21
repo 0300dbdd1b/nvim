ec3b02047d68d006360756d80b15da33d872e435
LesLie <noah.gr.addino@gmail.com>
2024-10-21 11:20:27 +0200

    Oil floating params

--- lua/plugins/keybinds/oil.lua
+++ lua/plugins/keybinds/oil.lua
@@ -1,2 +1,4 @@
 
-vim.keymap.set("n", "<leader>o", ":Oil<CR>", { desc = "Open Oil file explorer" })
+vim.keymap.set("n", "<leader>O", ":Oil<CR>", { desc = "Open Oil file explorer" })
+vim.keymap.set("n", "<leader>o", ":Oil --float<CR>", { desc = "Open Oil file explorer" })
+
